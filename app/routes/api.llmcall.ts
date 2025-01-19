afb0b9aa55389d5f3fefa79dd8d880c068c42058
jasonmatthewrowe <jason.rowe@pobox.com>
2025-01-19 10:53:24 -0600

    Update api.llmcall.ts

--- app/routes/api.llmcall.ts
+++ app/routes/api.llmcall.ts
@@ -1,4 +1,4 @@
-import { type ActionFunctionArgs } from '@remix-run/cloudflare';
+import { type ActionFunctionArgs } from '@remix-run/node';
 import { streamText } from '~/lib/.server/llm/stream-text';
 import type { IProviderSetting, ProviderInfo } from '~/types/model';
 import { generateText } from 'ai';
@@ -21,7 +21,7 @@ async function getModelList(options: {
   return llmManager.updateModelList(options);
 }
 
-async function llmCallAction({ context, request }: ActionFunctionArgs) {
+async function llmCallAction({ request }: ActionFunctionArgs) {
   const { system, message, model, provider, streamOutput } = await request.json<{
     system: string;
     message: string;
@@ -63,7 +63,7 @@ async function llmCallAction({ context, request }: ActionFunctionArgs) {
             content: `${message}`,
           },
         ],
-        env: context.cloudflare.env,
+        env: process.env,
         apiKeys,
         providerSettings,
       });
@@ -91,7 +91,7 @@ async function llmCallAction({ context, request }: ActionFunctionArgs) {
     }
   } else {
     try {
-      const models = await getModelList({ apiKeys, providerSettings, serverEnv: context.cloudflare.env as any });
+      const models = await getModelList({ apiKeys, providerSettings, serverEnv: process.env });
       const modelDetails = models.find((m: ModelInfo) => m.name === model);
 
       if (!modelDetails) {
@@ -116,7 +116,7 @@ async function llmCallAction({ context, request }: ActionFunctionArgs) {
         ],
         model: providerInfo.getModelInstance({
           model: modelDetails.name,
-          serverEnv: context.cloudflare.env as any,
+          serverEnv: process.env,
           apiKeys,
           providerSettings,
         }),
